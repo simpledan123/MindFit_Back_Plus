6a312c7f734d06fc48807c5a853e38a6541ce29a
simpledan <103040380+simpledan123@users.noreply.github.com>
2025-05-08 14:33:20 +0900

    Update routers.py

--- api/v1/routers.py
+++ api/v1/routers.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
-from api.v1.endpoints import restaurants, reviews, users, auth
-from api.v1.endpoints import chatbot
+from api.v1.endpoints import chatbot, auth, restaurants, reviews, users
 
 api_router = APIRouter()
+
+# 각 기능별 라우터 등록
+api_router.include_router(chatbot.router, prefix="/chatbot", tags=["chatbot"])
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
-api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(restaurants.router, prefix="/restaurants", tags=["restaurants"])
 api_router.include_router(reviews.router, prefix="/reviews", tags=["reviews"])
+api_router.include_router(users.router, prefix="/users", tags=["users"])
