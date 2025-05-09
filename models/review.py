f4ffd5c10b4a4882bf09446adf2f2268eb45078c
simpledan <103040380+simpledan123@users.noreply.github.com>
2025-05-09 10:13:06 +0900

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,15 +1,16 @@
+# MindFit_Back_Plus-main/models/review.py
 from sqlalchemy import Column, ForeignKey, Integer, String, Float
 from sqlalchemy.orm import relationship
 from db.database import Base
 
 class Review(Base):
     __tablename__ = "reviews"
-    
+
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id", ondelete="SET NULL"))
     restaurant_id = Column(Integer, ForeignKey("restaurants.id", ondelete="CASCADE"), nullable=False)
     rating = Column(Float, nullable=False, default=0.0)
     comment = Column(String, nullable=False)
 
-    user = relationship("User", backref="reviews")
-    restaurant = relationship("Restaurant", backref="reviews")
+    user = relationship("User", back_populates="reviews")
+    restaurant = relationship("Restaurant", back_populates="reviews")
